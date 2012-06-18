bfd3d62c1fe9c9b54bd50c4f0bd522f3b53be6a8
Jonns Mostovoys <amarr.industrial@gmail.com>
2012-06-18 15:38:25 +0300

    removed debug output from _lang

--- site/src/saloon_lang.erl
+++ site/src/saloon_lang.erl
@@ -12,9 +12,7 @@
 %%spawnpoint
 
 t([{_,X}]) -> 
-	%?debugFmt("Falling back to translation generator. String: <<\"~ts\">>", [X]),
 	Fname = ?SRCPATH ++ saloon_util:to_list(?MODULE) ++ ".erl",
-	?debugFmt("Filename: ~ts", [Fname]),
 	{_, Myself} = file:read_file(Fname),
 	NewFunText = io_lib:format("&~n~nt([{_,<<\"~ts\">>}]) -> ~n	case saloon_ctx:language() of ~n", [X]),
 	NewFunText2= lists:foldl(fun (L, Acc) -> Acc ++ io_lib:format("		~ts -> <<\"~ts\">>;~n", [L, X]) end, "", languages()),
@@ -25,7 +23,7 @@ t([{_,X}]) ->
 		unicode:characters_to_binary(NewFunText ++ NewFunText2 ++ NewFunText3)
 	),
 	file:write_file("site/src/saloon_lang.erl", NewerSelf),
-	?debugFmt("I'm compiling myself: ~p",[compile:file(Fname, [verbose,report_errors,report_warnings,{outdir,?BINPATH}])]),
+	compile:file(Fname, [verbose,report_errors,report_warnings,{outdir,?BINPATH}]),
 	%os:cmd("erlc " ++ Fname ++ " -o " ++ ?BINPATH), %%TODO: change to compile:file!
 	code:purge(saloon_lang),
 	code:load_file(saloon_lang),
